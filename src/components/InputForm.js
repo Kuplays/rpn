5769a86048f351154a7707c3b7b5b5df39dfadb3
Alex Kuplays <alexkuplays@gmail.com>
2020-12-13 18:55:16 +0700

    last checks

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -18,26 +18,41 @@ class InputForm extends React.Component {
         );
     }
 
+    isInputValid = () => {
+        let currentInputCheck = this.state.inputValue;
+        let isValid = true;
+        let validRegexp = /^[0-9\+\-\*\/\(\)]+$/;
+
+        if (currentInputCheck === "") {
+            isValid = false;
+        } else if (!currentInputCheck.match(validRegexp)) {
+            isValid = false;
+        }
+
+        return isValid;
+    }
+
     onSubmitForm = (e) => {
         e.preventDefault();
         let val = encodeURIComponent(this.state.inputValue.replace(/\s/g, ""));
         console.log("ENCODED AS: " + val);
-        fetch("http://localhost:8080/api?infixNotation=" + val)
-        .then(res => res.json())
-        .then(
-            (result) => {
-                console.log("PASSING TO PARENT: " + result);
-                this.props.passResult(result);
-                this.props.parentToggler(true);
-                console.log("SUCCESS: " + result.infixNotation);
-                console.log("SUCCESS: " + result.transformedNotation);
-                console.log("SUCCESS: " + result.result);
-            },
-            (error) => {
-                this.props.parentToggler(false);
-                console.log("ERROR: " + error.message );
-            }
-        )
+        if (this.isInputValid())
+            fetch("https://kuplays-rpn.herokuapp.com/api?infixNotation=" + val)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log("PASSING TO PARENT: " + result);
+                    this.props.passResult(result);
+                    this.props.parentToggler(true);
+                },
+                (error) => {
+                    this.props.parentToggler(false);
+                    console.log("ERROR: " + error.message );
+                }
+            )
+        else {
+            alert("Input contains errors");
+        }
     }
 
     render() {
@@ -45,7 +60,7 @@ class InputForm extends React.Component {
             <Form onSubmit={this.onSubmitForm}>
                 <FormGroup>
                     <Label for="inputValue">INFIX NOTATION</Label>
-                    <Input className="text-center" type="text" name="inputInfix" id="inputValue" onChange={this.onChangeInput} placeholder="(5 + 5) / 2" />
+                    <Input className="text-center" type="text" name="inputInfix" id="inputValue" onChange={this.onChangeInput} placeholder="6 / 2 * (2 + 1)" />
                 </FormGroup>
                 <Button className="col-12 btn-success">SUBMIT</Button>
             </Form>
